a190b6f17bcb12581f6fff265506a05972f3ca5e
Junren.Chen <Junren.Chen@desay-svautomotive.com>
2020-12-31 14:23:26 +0800

    [ActivityLifeCycleTest][the demo to understand the life-cycle of Activity]
    Signed-off-by: Junren.Chen <Junren.Chen@desay-svautomotive.com>

---
+++ ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.activitylifecycletest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null) {
+            String tempdata = savedInstanceState.getString("data_key");
+            Log.d(TAG, tempdata);
+        }
+
+        Button button_startNormalActivity=(Button) findViewById(R.id.start_normal_activity);
+        Button button_startDialogAvtivity=(Button) findViewById(R.id.start_dialog_activity);
+        button_startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+        button_startDialogAvtivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    // 该方法在 Activity 回收之前会被调用，用于保存之前的数据状态
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        String tempdata = "Something you just typed";
+        outState.putString("data_key", tempdata);
+    }
+}
