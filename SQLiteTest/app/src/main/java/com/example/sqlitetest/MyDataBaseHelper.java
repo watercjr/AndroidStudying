c51a3d753cd2be8a7f6748aca68cbb1e1b9cea14
Junren.Chen <Junren.Chen@desay-svautomotive.com>
2021-03-04 15:10:01 +0800

    [SQLiteTest][add ContentProvider for ProviderTest to use the data]

--- SQLiteTest/app/src/main/java/com/example/sqlitetest/MyDataBaseHelper.java
+++ SQLiteTest/app/src/main/java/com/example/sqlitetest/MyDataBaseHelper.java
@@ -34,7 +34,7 @@ public class MyDataBaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREAT_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
